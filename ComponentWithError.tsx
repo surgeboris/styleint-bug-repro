87254ab51d975eccdc6da9c8e6d7c4910461dba8
Boris Sergeyev <boris.sergeyev@quoscient.io>
2019-08-23 14:36:07 +0200

    Reproduced stylelint CLI bug: wrong process used in case of stdin input
    ```
    stylelint-bug-repro$ npm run stdin
    
    ComponentWithError.tsx
     6:5  ✖  Unexpected unknown property "wuidth"   property-no-unknown
    
    stylelint-bug-repro$ npm run filename
    
    <input css 1>
     3:77  ✖  Unexpected token (3:77)
    ```

---
+++ ComponentWithError.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const tsSyntaxThatBreaksDefaultParser = createStandardAction('FETCHED')<Data[]>()[0]!;
+
+export default styled.div`
+    wuidth: 100%;
+`;
---
+++ README.md
@@ -0,0 +1,7 @@
+```shell
+$ git clone https://github.com/surgeboris/styleint-bug-repro
+$ cd stylelint-bug-repro
+$ npm ci
+$ npm run stdin
+$ npm run filename
+```
